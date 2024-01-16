21d448d2b0ec1082bde8c4dfd0f5dc2484c12e72
W Song <wei.song@senecacollege.ca>
2024-01-16 16:28:07 -0500

    updated 2237/companiesDB.js for 2241

--- web422/download/2237/companiesDB.js
+++ web422/download/2237/companiesDB.js
@@ -2,49 +2,49 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const companySchema = new Schema({
-    "name": String,
-    "permalink": String,
-    "crunchbase_url": String,
-    "homepage_url": String,
-    "blog_url": String,
-    "blog_feed_url": String,
-    "twitter_username": String,
-    "category_code": String,
-    "number_of_employees": Number,
-    "founded_year": Number,
-    "founded_month": Number,
-    "founded_day": Number,
-    "deadpooled_year": Number,
-    "deadpooled_month": Number,
-    "deadpooled_day": Number,
-    "deadpooled_url": String,
-    "tag_list": String,
-    "alias_list": String,
-    "email_address": String,
-    "phone_number": String,
-    "description": String,
+  "name": String,
+  "permalink": String,
+  "crunchbase_url": String,
+  "homepage_url": String,
+  "blog_url": String,
+  "blog_feed_url": String,
+  "twitter_username": String,
+  "category_code": String,
+  "number_of_employees": Number,
+  "founded_year": Number,
+  "founded_month": Number,
+  "founded_day": Number,
+  "deadpooled_year": Number,
+  "deadpooled_month": Number,
+  "deadpooled_day": Number,
+  "deadpooled_url": String,
+  "tag_list": String,
+  "alias_list": String,
+  "email_address": String,
+  "phone_number": String,
+  "description": String,
 
-    "overview": String,
-    "image": {},
-    "products": [],
-    "relationships": [],
-    "competitions": [],
-    "providerships": [],
-    "total_money_raised": String,
-    "funding_rounds": [],
-    "investments": [],
-    "acquisition": {},
-    "acquisitions": [],
-    "offices": [],
-    "milestones": [],
-    "ipo": {},
-    "video_embeds": [],
-    "screenshots": [],
-    "external_links": [{
-        "external_url": String,
-        "title": String
-    }],
-    "partners": []
+  "overview": String,
+  "image": {},
+  "products": [],
+  "relationships": [],
+  "competitions": [],
+  "providerships": [],
+  "total_money_raised": String,
+  "funding_rounds": [],
+  "investments": [],
+  "acquisition": {},
+  "acquisitions": [],
+  "offices": [],
+  "milestones": [],
+  "ipo": {},
+  "video_embeds": [],
+  "screenshots": [],
+  "external_links": [{
+    "external_url": String,
+    "title": String
+  }],
+  "partners": []
 });
 
 module.exports = class companiesDB {
@@ -80,26 +80,26 @@ module.exports = class companiesDB {
     return newCompany;
   }
 
-  getAllCompanies(page, perPage, tag) {
-    let findBy = tag ? { tag_list: new RegExp(tag, "i") } : {};
+  getAllCompanies(page, perPage, name) {
+    let findBy = name ? { name: new RegExp(name, "i") } : {};
 
     if (+page && +perPage) {
-      return this.Company.find(findBy).sort({ year: +1 }).skip((page - 1) * +perPage).limit(+perPage).exec();
+      return this.Company.find(findBy).sort({ founded_year: -1, founded_month: -1, founded_day: -1 }).skip((page - 1) * +perPage).limit(+perPage).exec();
     }
 
     return Promise.reject(new Error('page and perPage query parameters must be valid numbers'));
   }
 
-  getCompanyByName(name) {
-    // return this.Company.findOne({ name: name }).exec();
-    return this.Company.findOne({ name: new RegExp('^' + name + '$', "i") }).exec();
+  getCompanyById(id) {
+    return this.Company.findOne({ _id: id }).exec();
+    // return this.Company.findOne({ name: new RegExp('^' + name + '$', "i") }).exec();
   }
 
-  updateCompanyByName(data, name) {
-    return this.Company.updateOne({ name: name }, { $set: data }).exec();
+  updateCompanyById(data, id) {
+    return this.Company.updateOne({ _id: id }, { $set: data }).exec();
   }
 
-  deleteCompanyByName(name) {
-    return this.Company.deleteOne({ name: name }).exec();
+  deleteCompanyById(id) {
+    return this.Company.deleteOne({ _id: id }).exec();
   }
 }
